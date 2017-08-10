546cff74fa36b43c41c5969164b6af10d19c4140
daanwissing <daan.wissing@gmail.com>
2017-08-10 12:05:22 +0200

    Run all solutions with npm start

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
-var advent_id = 0;
+var advent_id = process.argv[2];
+var adventDir = './advents/';
 
-process.argv.forEach((val) => {
-  advent_id = val;
-})
+if (advent_id) {
+  // Check if the advent is present
+  if (!fs.existsSync(adventDir + advent_id + '.js')) {
+    console.log('Event ' + advent_id + ' not found');
+    return;
+  }
 
-// Check if the advent is present
-if (!fs.existsSync('advents/' + advent_id + '.js')) throw 'Event ' + advent_id + ' not found'
-
-// Include the event, running it immediately
-var Sol = require('./advents/' + advent_id);
-var adv = new Sol();
-adv.run();
+  // Include the event, running it immediately
+  var Sol = require(adventDir + advent_id);
+  var adv = new Sol();
+  adv.run();
+} else {
+  // Run all of the events
+  fs.readdir(adventDir, (err, files) => {
+    files.forEach((file) => {
+      var path = adventDir + file;
+      if (fs.lstatSync(path).isDirectory()) return;
+      var Sol = require(path);
+      adv = new Sol();
+      adv.run();
+    });
+  });
+};
